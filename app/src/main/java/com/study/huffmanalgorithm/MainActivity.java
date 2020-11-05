b229a6489823b72844357b5ac86ea0dad382d045
lia <lkno01@daum.net>
2020-11-05 16:15:44 +0900

    create new function

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -8,6 +8,9 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.ByteArrayOutputStream;
+import java.math.BigInteger;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG="Huffman";
@@ -21,4 +24,23 @@ public class MainActivity extends AppCompatActivity {
         Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
         Log.d(TAG, "bitmap 변환 : "+img_bitmap);
     }
+
+    void bitmap_to_hex() {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 8;
+        Resources res=getResources();
+        Bitmap receipt= BitmapFactory.decodeResource(res,R.drawable.sample);
+        int size = receipt.getRowBytes() * receipt.getHeight();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream);
+        byte[] receiptbyte = stream.toByteArray();
+        String hexstring = toHex(receiptbyte);
+    }
+
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        return String.format("%0" + (bytes.length << 1) + "X", bi);
+    }
+
+
 }
