7ac7d5ef8a8af075881d5b8626c84070e7407a85
lia <lkno01@daum.net>
2020-11-05 14:46:28 +0900

    commit test

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -2,13 +2,23 @@ package com.study.huffmanalgorithm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="Huffman";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res=getResources();
+        Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
+        Log.d(TAG, "bitmap 변환 : "+img_bitmap);
     }
 }
