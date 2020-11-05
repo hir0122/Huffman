740fda2e04b4737c9eb20814742c658d828aab3d
lia <lkno01@daum.net>
2020-11-05 16:30:10 +0900

    first run

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -20,25 +20,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resources res=getResources();
-        Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
-        Log.d(TAG, "bitmap 변환 : "+img_bitmap);
+        bitmap_to_hex();
+//        Resources res=getResources();
+//        Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
+//        Log.d(TAG, "bitmap 변환 : "+img_bitmap);
     }
+
 
     void bitmap_to_hex() {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 8;
         Resources res=getResources();
         Bitmap receipt= BitmapFactory.decodeResource(res,R.drawable.sample);
+        Log.d(TAG, "bitmap 변환 : " + receipt);
         int size = receipt.getRowBytes() * receipt.getHeight();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream);
         byte[] receiptbyte = stream.toByteArray();
+        Log.d(TAG, "receiptbyte : " + receiptbyte);
         String hexstring = toHex(receiptbyte);
     }
 
     public static String toHex(byte[] bytes) {
         BigInteger bi = new BigInteger(1, bytes);
+        Log.d(TAG, "toHex : " + String.format("%0" + (bytes.length << 1) + "X", bi));
         return String.format("%0" + (bytes.length << 1) + "X", bi);
     }
 
