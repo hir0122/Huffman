0920500ab62823100f2df9bfe02b5ab0d92b5b63
lia <lkno01@daum.net>
2020-11-05 18:24:08 +0900

    typing

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -18,10 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG="Huffman";
 
-
-
-
-
     public static PriorityQueue<Node> queue; // 우선순위 큐
     public static HashMap<Character, String> charToCode=new HashMap<Character, String>(); // 문자에 따른 코드 값 해시맵 할당
 
@@ -34,10 +30,41 @@ public class MainActivity extends AppCompatActivity {
 //        Resources res=getResources();
 //        Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
 //        Log.d(TAG, "bitmap 변환 : "+img_bitmap);
+    HashMap <Character,Integer> dictionary=new HashMap<Character, Integer>(); // 각각의 문자에 대한 빈도수 체크
+    for(int i=0; i<bitmap_to_hex().length(); i++){
+        char temp=bitmap_to_hex().charAt(i);
+        if(dictionary.containsKey(temp)) // 현재 문자가 이미 1번 이상 들어가 있다면 1 증가
+            dictionary.put(temp,dictionary.get(temp)+1);
+        else
+            dictionary.put(temp,1);
     }
+    // 모든 노드를 우선순위 큐에 추가함으로써 트리 그룹 형성
+        queue=new PriorityQueue<Node>(1000, new FrequencyComparator());
+    int number=0;
+
+    //문자와 그 빈도수가 저장된 각각의 모든 노드들을 우선순위 큐에 삽입
+        for(Character c:dictionary.keySet()){
+            Node temp=new Node();
+            temp.character=c;
+            temp.frequency=dictionary.get(c);
+            queue.add(temp);
+            number++;
+        }
+
+        //전체 노드 개수만큼 재배열하여 우선순위 큐 상에서의 노드 재배열
+        Node root=huffmanCoding(number);
+        //변수 길이 코드를 생성
+        traversal(root, new String());
+
+        String result=new String();
+        for(int i=0; i<bitmap_to_hex().length(); i++){
+            result=result+charToCode.get(bitmap_to_hex().charAt(i))+" ";
+        }
+
+   }
 
 
-    void bitmap_to_hex() {
+    String bitmap_to_hex() {
         BitmapFactory.Options options = new BitmapFactory.Options();
         options.inSampleSize = 8;
         Resources res=getResources();
@@ -48,14 +75,39 @@ public class MainActivity extends AppCompatActivity {
         receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream);
         byte[] receiptbyte = stream.toByteArray();
         Log.d(TAG, "receiptbyte : " + receiptbyte);
-        String hexstring = toHex(receiptbyte);
+        //String hexstring = toHex(receiptbyte);
+
+        BigInteger bi = new BigInteger(1, receiptbyte);
+        Log.d(TAG, "toHex : " + String.format("%0" + (receiptbyte.length << 1) + "X", bi));
+        String hexstring=  String.format("%0" + (receiptbyte.length << 1) + "X", bi);
+        return hexstring;
     }
 
-    public static String toHex(byte[] bytes) {
-        BigInteger bi = new BigInteger(1, bytes);
-        Log.d(TAG, "toHex : " + String.format("%0" + (bytes.length << 1) + "X", bi));
-        return String.format("%0" + (bytes.length << 1) + "X", bi);
+//    public static String toHex(byte[] bytes) {
+//        BigInteger bi = new BigInteger(1, bytes);
+//        Log.d(TAG, "toHex : " + String.format("%0" + (bytes.length << 1) + "X", bi));
+//        return String.format("%0" + (bytes.length << 1) + "X", bi);
+//    }
+
+    public static Node huffmanCoding(int n){
+        // 각각의 문자 빈도수에 따라서 트리를 건축하는 메소드
+        for(int i=0; i<n-1; i++){
+            Node node=new Node();
+            node.right=queue.poll();
+            node.left=queue.poll();
+            node.frequency=node.right.frequency+node.left.frequency;
+            queue.add(node);
+        }
+        return queue.poll();
     }
 
-
+    //순회로 노드를 돌아 코드를 입력력
+   public static void traversal(Node n, String s){
+        if(n==null) return;
+        traversal(n.left,s+"0");
+        traversal(n.right,s+"1");
+        if(n.character!='\0'){
+            charToCode.put(n.character,s);
+        }
+    }
 }
