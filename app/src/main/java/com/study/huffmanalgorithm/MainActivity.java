2de0b2dacad4ca0be36d43025caf06a38254b83b
lia <lkno01@daum.net>
2020-11-06 12:29:05 +0900

    Log.d(TAG, "Node root : " + root); > Node root = null

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -24,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     public static PriorityQueue<Node> queue; // 우선순위 큐
     public static HashMap<Character, String> charToCode=new HashMap<Character, String>(); // 문자에 따른 코드 값 해시맵 할당
 
+    String hexstring;
+
     String[] hexa2bin = {"0000", "0001", "0010", "0011",
 
             "0100", "0101", "0110", "0111",
@@ -56,13 +58,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String stringHex=bitmap_to_hex();
+        hexstring=bitmap_to_hex();
+        Log.d(TAG, "hexstring : "+hexstring);
 
-        binary = new String[stringHex.length()][bin2hexa.length];
+//        binary = new String[stringHex.length()][bin2hexa.length];
 
-        Log.d(TAG, "16진수 : " + stringHex);
-//
-//
 //        //hex를 2진수로
 //        for (int i=0; i< stringHex.length(); i++){
 //            for(int j=0; j <bin2hexa.length; j++) {
@@ -81,38 +81,42 @@ public class MainActivity extends AppCompatActivity {
 //
 //        Log.d(TAG, "2진수 : " + binary_to_hex);
 
-//        Resources res=getResources();
-//        Bitmap img_bitmap= BitmapFactory.decodeResource(res,R.drawable.sample);
-//        Log.d(TAG, "bitmap 변환 : "+img_bitmap);
-//    HashMap <Character,Integer> dictionary=new HashMap<Character, Integer>(); // 각각의 문자에 대한 빈도수 체크
-//    for(int i=0; i<bitmap_to_hex().length(); i++){
-//        char temp=bitmap_to_hex().charAt(i);
-//        if(dictionary.containsKey(temp)) // 현재 문자가 이미 1번 이상 들어가 있다면 1 증가
-//            dictionary.put(temp,dictionary.get(temp)+1);
-//        else
-//            dictionary.put(temp,1);
-//    }
-//    // 모든 노드를 우선순위 큐에 추가함으로써 트리 그룹 형성
-//        queue=new PriorityQueue<Node>(1000, new FrequencyComparator());
-//    int number=0;
-//
-//    //문자와 그 빈도수가 저장된 각각의 모든 노드들을 우선순위 큐에 삽입
-//        for(Character c:dictionary.keySet()){
-//            Node temp=new Node();
-//            temp.character=c;
-//            temp.frequency=dictionary.get(c);
-//            queue.add(temp);
-//            number++;
-//        }
-//
-//        //전체 노드 개수만큼 재배열하여 우선순위 큐 상에서의 노드 재배열
-//        Node root=huffmanCoding(number);
-//        //변수 길이 코드를 생성
-//        traversal(root, new String());
-//
-//        String result=new String();
-//        for(int i=0; i<bitmap_to_hex().length(); i++){
-//            result=result+charToCode.get(bitmap_to_hex().charAt(i))+" ";
+    HashMap <Character,Integer> dictionary=new HashMap<Character, Integer>(); // 각각의 문자에 대한 빈도수 체크
+
+    for(int i=0; i<hexstring.length(); i++){ // dictionary에 A, B, C, D, E, F, 0~9까지 빈도수가 들어간다
+        char temp=hexstring.charAt(i);
+        if(dictionary.containsKey(temp)) // 현재 문자가 이미 1번 이상 들어가 있다면 1 증가
+            dictionary.put(temp,dictionary.get(temp)+1);
+        else
+            dictionary.put(temp,1);
+
+        //Log.d(TAG, "dictionary : " + dictionary);
+    }
+    // 모든 노드를 우선순위 큐에 추가함으로써 트리 그룹 형성
+        queue=new PriorityQueue<Node>(hexstring.length(), new FrequencyComparator());
+    int number=0;
+
+    //문자와 그 빈도수가 저장된 각각의 모든 노드들을 우선순위 큐에 삽입
+        for(Character c:dictionary.keySet()){
+            Node temp=new Node();
+            temp.character=c;
+            temp.frequency=dictionary.get(c);
+            queue.add(temp);
+            number++;
+            //Log.d(TAG, "character : " + temp.character + "\nfrequency : "+temp.frequency);
+        }
+
+        Log.d(TAG, "number : " + number);
+
+        //전체 노드 개수만큼 재배열하여 우선순위 큐 상에서의 노드 재배열
+        Node root=huffmanCoding(number);
+        Log.d(TAG, "Node root : " + root);
+        //변수 길이 코드를 생성
+        traversal(root, new String());
+
+//        String result="";
+//        for(int i=0; i<hexstring.length(); i++){
+//            result=result+charToCode.get(hexstring.charAt(i))+" ";
 //            Log.d(TAG, "result : " + result);
 //        }
 
@@ -126,22 +130,29 @@ public class MainActivity extends AppCompatActivity {
         Resources res=getResources();
         Bitmap receipt= BitmapFactory.decodeResource(res,R.drawable.sample);
 
-        Log.d(TAG, "bitmap 변환 : " + receipt);
+        //Log.d(TAG, "bitmap 변환 : " + receipt);
         int size = receipt.getRowBytes() * receipt.getHeight();
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
         receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream);
-        Log.d(TAG, "압축?? : " + receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream));
+        //Log.d(TAG, "압축?? : " + receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream));
         byte[] receiptbyte = stream.toByteArray();
 
-        Log.d(TAG, "receiptbyte : " + receiptbyte);
+        //Log.d(TAG, "receiptbyte : " + receiptbyte);
         //String hexstring = toHex(receiptbyte);
 
         BigInteger bi = new BigInteger(1, receiptbyte);
         Log.d(TAG, "toHex : " + String.format("%0" + (receiptbyte.length << 1) + "X", bi));
-        String hexstring=  String.format("%0" + (receiptbyte.length << 1) + "X", bi);
-        return hexstring;
+        //hexstring=  String.format("%0" + (receiptbyte.length << 1) + "X", bi);
+
+        return String.format("%0" + (receiptbyte.length << 1) + "X", bi);
     }
+
+//    int hex_to_string(){
+//        int hex2dec = Integer.parseInt(bitmap_to_hex(), 16);
+//        Log.d(TAG, "toHex : " + hex2dec);
+//        return hex2dec;
+//    }
 
 //    public static String toHex(byte[] bytes) {
 //        BigInteger bi = new BigInteger(1, bytes);
@@ -158,10 +169,11 @@ public class MainActivity extends AppCompatActivity {
             node.frequency=node.right.frequency+node.left.frequency;
             queue.add(node);
         }
+        Log.d(TAG, "result : " + queue.poll());
         return queue.poll();
     }
 
-    //순회로 노드를 돌아 코드를 입력력
+    //순회로 노드를 돌아 코드를 입력
    public static void traversal(Node n, String s){
         if(n==null) return;
         traversal(n.left,s+"0");
@@ -169,5 +181,6 @@ public class MainActivity extends AppCompatActivity {
         if(n.character!='\0'){
             charToCode.put(n.character,s);
         }
+
     }
 }
