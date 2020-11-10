9a38fa0098222f9f46430084adcccf855e4bb283
lia <lkno01@daum.net>
2020-11-10 20:20:36 +0900

    2020-11-10 20:13:21.329 7871-7871/com.study.huffmanalgorithm D/Huffman: Original data size : 4127528Bit (515941Byte) 2020-11-10 20:13:21.331 7871-7871/com.study.huffmanalgorithm D/Huffman: Encoded data size : 3116424bit (390 kBByte)

--- app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
+++ app/src/main/java/com/study/huffmanalgorithm/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Huffman";
 
     String hexstring;
+    int hexint=0;
 
     private static Map<Character, String> prefixCodeTable = new HashMap<>();    // Binary PrefixCode by Character (HashMap)
 
@@ -54,39 +55,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Resources res = getResources();
-        Bitmap receipt = BitmapFactory.decodeResource(res, R.drawable.sample);
-        hexstring=BitmapToString(receipt);
+        BitmapFactory.Options options=new BitmapFactory.Options();
+        //options.inSampleSize=1;
+        Bitmap src=BitmapFactory.decodeResource(getResources(),R.drawable.sample,options);
+        Bitmap resized=Bitmap.createScaledBitmap(src, 700, 616, true);
+        hexstring=BitmapToString(resized);
+        long size=resized.getWidth()*resized.getHeight()*24/8;
+        String size1=android.text.format.Formatter.formatFileSize(this, size);
+        Log.d(TAG, "원본 크기zfsdfsfre : " + size1);
 
-        Log.d(TAG, "Original data : " + this.hexstring);
+
+//        Resources res = getResources();
+//        Bitmap receipt = BitmapFactory.decodeResource(res, R.drawable.sample2); // 이미지 가져오기
+//
+//        hexstring=bitmap_to_hex(receipt); // 이미지를 16진수로 변환
+
+        //Log.d(TAG, "Original data : " + this.hexstring);
+        int OriginSize= hexstring.length();
+        Log.d(TAG, "원본 크기 : " + OriginSize);
 
         // Huffman Encoding
-        String encodeData = encode(hexstring);
+        String encodeData = encode(hexstring); // 이미지 압축 해제
         Log.d(TAG, "Encoded data : " + encodeData);
+        Log.d(TAG, "hexint data : " + hexint);
 
-        Double size= ((encodeData.length())/1024.0)/8.0;
-        Log.d(TAG, "크기 : " + size);
+        Double EncodeSize= ((encodeData.length())/1024.0);
+        Log.d(TAG, "Encode 후 크기 : " + EncodeSize);
 
         // Huffman Decoding
         String decodeData = decode(encodeData);
-        Log.d(TAG, "Decoded data : " + decodeData + "\n");
+        Log.d(TAG, "Decoded data : " + decodeData + "\n"); // 이미지 다시 압축
+
+        Double DecodeSize= ((decodeData.length())/1024.0)/8.0;
+        Log.d(TAG, "Decode 후 크기 : " + DecodeSize);
 
         // Print Report
         int originDataByteSize = hexstring.getBytes(StandardCharsets.UTF_8).length;
         Log.d(TAG, "Original data size : " + originDataByteSize * 8 + "Bit (" + originDataByteSize + "Byte)");
-        int encodeDataByteSize = encodeData.length() % 8 == 0 ? encodeData.length() / 8 : encodeData.length() / 8 + 1;
+        //int encodeDataByteSize = encodeData.length() % 8 == 0 ? encodeData.length() / 8 : encodeData.length() / 8 + 1;
+        long encodeByte=encodeData.length()/8;
+        String encodeDataByteSize=android.text.format.Formatter.formatFileSize(this, encodeByte);
         Log.d(TAG, "Encoded data size : " + encodeData.length() + "bit (" + encodeDataByteSize + "Byte)");
     }
 
     // Encoding method
-    public static String encode(String data) {
+    public String encode(String data) { // encode 안에 hexstring의 크기가 들어있다
         // Get Frequency by Character
         Map<Character, Integer> charFreq = new HashMap<>();
         for (char c : data.toCharArray()) {
@@ -96,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
                 int no = charFreq.get(c);
                 charFreq.put(c, ++no);
             }
+            hexint++;
         }
         Log.d(TAG, "Frequency by Character : " + charFreq);
 
@@ -190,37 +210,35 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-//    String bitmap_to_hex(Bitmap receipt) {
-//        BitmapFactory.Options options = new BitmapFactory.Options();
-//        //options.inSampleSize = 1;
-//
-//        //Resources res=getResources();
-//        //Bitmap receipt= BitmapFactory.decodeResource(res,R.drawable.sample2);
-//
-//        //Log.d(TAG, "bitmap 변환 : " + receipt);
-//        int size = receipt.getRowBytes() * receipt.getHeight();
-//        ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//
-//        receipt.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//        //Log.d(TAG, "압축?? : " + receipt.compress(Bitmap.CompressFormat.JPEG, 90, stream));
-//        byte[] receiptbyte = stream.toByteArray();
-//
-//        Log.d(TAG, "receiptbyte : " + receiptbyte);
-//
-//        //String hexstring = toHex(receiptbyte);
-//
-//        BigInteger bi = new BigInteger(1, receiptbyte);
-//        Log.d(TAG, "toHex : " + String.format("%0" + (receiptbyte.length << 1) + "X", bi));
-//        //hexstring=  String.format("%0" + (receiptbyte.length << 1) + "X", bi);
-//
-//        return String.format("%0" + (receiptbyte.length << 1) + "X", bi);
-//    }
+    String bitmap_to_hex(Bitmap receipt) {
+        //BitmapFactory.Options options = new BitmapFactory.Options();
+        //options.inSampleSize = 1;
+
+        int size = receipt.getRowBytes() * receipt.getHeight();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        receipt.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+        byte[] receiptbyte = stream.toByteArray();
+
+        Log.d(TAG, "receiptbyte : " + receiptbyte);
+
+        BigInteger bi = new BigInteger(1, receiptbyte);
+        String format = String.format("%0" + (receiptbyte.length << 1) + "X", bi);
+        Log.d(TAG, "toHex : " + format);
+
+        return format;
+    }
 
     public String BitmapToString(Bitmap bitmap) {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
         byte[] bytes = baos.toByteArray();
         String temp = Base64.encodeToString(bytes, Base64.DEFAULT);
+        String imageString = Base64.encodeToString(bytes, Base64.DEFAULT);
+
+        //decode base64 string to image
+        bytes = Base64.decode(imageString, Base64.DEFAULT);
+        //Bitmap decodedImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
         return temp;
     }
 
@@ -230,10 +248,10 @@ public class MainActivity extends AppCompatActivity {
 //        return hex2dec;
 //    }
 
-//    public static String toHex(byte[] bytes) {
-//        BigInteger bi = new BigInteger(1, bytes);
-//        Log.d(TAG, "toHex : " + String.format("%0" + (bytes.length << 1) + "X", bi));
-//        return String.format("%0" + (bytes.length << 1) + "X", bi);
-//    }
+    public static String toHex(byte[] bytes) {
+        BigInteger bi = new BigInteger(1, bytes);
+        Log.d(TAG, "toHex : " + String.format("%0" + (bytes.length << 1) + "X", bi));
+        return String.format("%0" + (bytes.length << 1) + "X", bi);
+    }
 
 }
